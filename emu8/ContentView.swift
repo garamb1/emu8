69e74e7d8df1de68593921726c2e4d89a837d03e
Vincenzo Garambone <garambo@gmail.com>
2024-08-01 11:14:40 +0200

    Project setup

---
+++ emu8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  emu8
+//
+//  Created by Vincenzo Garambone on 01/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ emu8/Emu8App.swift
@@ -0,0 +1,17 @@
+//
+//  emu8App.swift
+//  emu8
+//
+//  Created by Vincenzo Garambone on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct emu8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
